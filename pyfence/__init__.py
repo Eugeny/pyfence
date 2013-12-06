67825716b4e208fa657c17b2653228c8bd0da966
Eugene Pankov <john.pankov@gmail.com>
2013-12-06 15:40:02 +0300

    bump

--- pyfence/__init__.py
+++ pyfence/__init__.py
@@ -4,7 +4,7 @@ from pyfence.bootstrap import parse_options, install_import_hook
 from pyfence.core import fence_function, fence_namespace
 
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 options = defaultdict(bool)
